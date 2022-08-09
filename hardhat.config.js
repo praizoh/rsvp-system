644fdc3177200da2e8c1469b068d42a2936c31dd
praizoh <oremei.akande@gmail.com>
2022-08-09 10:14:00 +0100

    deploy and verify on mumbai network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+console.log(process.env)
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.STAGING_ALCHEMY_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY,
+  }
 };
